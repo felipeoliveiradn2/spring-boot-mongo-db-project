d4aab00b822f4c5b9a2760aebd396ce9f15282a8
Felipe de Oliveira <felipeoliveiradn2@gmail.com>
2025-10-25 13:41:37 -0300

    operation to instantiation of database

---
+++ src/main/java/com/felipeoliveira/mongodbproject/config/Instantiation.java
@@ -0,0 +1,29 @@
+package com.felipeoliveira.mongodbproject.config;
+
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Configuration;
+
+import com.felipeoliveira.mongodbproject.domain.User;
+import com.felipeoliveira.mongodbproject.repository.UserRepository;
+
+@Configuration
+public class Instantiation implements CommandLineRunner {
+
+	@Autowired
+	private UserRepository userRepository;
+	
+	@Override
+	public void run(String... args) throws Exception {
+		
+		userRepository.deleteAll();
+		
+		User maria = new User(null, "Maria Brown", "maria@gmail.com");
+		User alex = new User(null, "Alex Sanchez", "alex@gmail.com");
+		User bob = new User(null, "Bruno Jose", "jose@gmail.com");
+
+		userRepository.saveAll(Arrays.asList(maria, alex, bob));
+	}
+}
