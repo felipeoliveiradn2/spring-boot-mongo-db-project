6d328eea3e18ef465c1215d48c31b2a5114d8c6d
Felipe de Oliveira <felipeoliveiradn2@gmail.com>
2025-12-30 13:49:05 -0300

    User deletion with delete

--- src/main/java/com/felipeoliveira/mongodbproject/resources/UserResource.java
+++ src/main/java/com/felipeoliveira/mongodbproject/resources/UserResource.java
@@ -1,12 +1,16 @@
 package com.felipeoliveira.mongodbproject.resources;
+import java.net.URI;
 import java.util.List;
 import java.util.stream.Collectors;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
 import com.felipeoliveira.mongodbproject.domain.User;
 import com.felipeoliveira.mongodbproject.dto.UserDTO;
 import com.felipeoliveira.mongodbproject.services.UserService;
@@ -29,5 +33,22 @@ public class UserResource {
 	public ResponseEntity<UserDTO> findById(@PathVariable String id) {
 		User obj = service.findById(id);		
 		return ResponseEntity.ok().body(new UserDTO(obj));		
+	}
+	
+	@RequestMapping(method = RequestMethod.POST) // get to obtain information in rest projects
+	public ResponseEntity<Void> insert(@RequestBody UserDTO objDto) {
+		User obj = service.fromDTO(objDto);
+		obj = service.insert(obj);
+		//esse comando URI vai pegar o novo endereço do objeto inserido 
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId()).toUri();
+		//created retorna o código 201, que é o código de retorno http quando você cria um novo recurso
+		return ResponseEntity.created(uri).build();
+	}
+	
+	@RequestMapping(value="/{id}", method=RequestMethod.DELETE)
+	public ResponseEntity<Void> delete(@PathVariable String id) {
+		service.delete(id);
+		//noContent().build() irá retornar o código 204
+		return ResponseEntity.noContent().build();
 	}
 }
