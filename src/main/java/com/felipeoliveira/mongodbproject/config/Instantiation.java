337ffb2db12f1a44381dd829bc1ced8b9a0936e9
Felipe de Oliveira <felipeoliveiradn2@gmail.com>
2026-01-15 13:14:39 -0300

    Author's Date Projection with DTO

--- src/main/java/com/felipeoliveira/mongodbproject/config/Instantiation.java
+++ src/main/java/com/felipeoliveira/mongodbproject/config/Instantiation.java
@@ -10,6 +10,7 @@ import org.springframework.context.annotation.Configuration;
 
 import com.felipeoliveira.mongodbproject.domain.Post;
 import com.felipeoliveira.mongodbproject.domain.User;
+import com.felipeoliveira.mongodbproject.dto.AuthorDTO;
 import com.felipeoliveira.mongodbproject.repository.PostRepository;
 import com.felipeoliveira.mongodbproject.repository.UserRepository;
 
@@ -33,11 +34,12 @@ public class Instantiation implements CommandLineRunner {
 		User alex = new User(null, "Alex Sanchez", "alex@gmail.com");
 		User bob = new User(null, "Bruno Jose", "jose@gmail.com");
 
-		Post post1 = new Post(null, sdf.parse("21/03/2018"), "Partiu Viagem", "Vou viajar para São Paulo, abraços", maria);
-		Post post2 = new Post(null, sdf.parse("21/03/2018"), "Bom dia", "Acordei feliz hoje", maria);
+		userRepository.saveAll(Arrays.asList(maria, alex, bob));
+
+		Post post1 = new Post(null, sdf.parse("21/03/2018"), "Partiu Viagem", "Vou viajar para São Paulo, abraços", new AuthorDTO (maria));
+		Post post2 = new Post(null, sdf.parse("21/03/2018"), "Bom dia", "Acordei feliz hoje", new AuthorDTO (maria));
 		
 		postRepository.saveAll(Arrays.asList(post1, post2));		
-		userRepository.saveAll(Arrays.asList(maria, alex, bob));
 		
 		
 		
